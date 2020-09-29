c7cbe67193ad10f0210e85ecf7dc307bfc997afc
Chase May <chaseloganmay@gmail.com>
2020-09-29 12:24:47 -0600

    Add support for localStorage

--- modern-javascript/task-list/app.js
+++ modern-javascript/task-list/app.js
@@ -4,12 +4,34 @@ const taskList = document.querySelector('#task-list');
 const filter = document.querySelector('#filter-tasks');
 const clearBtn = document.querySelector('#clear-tasks');
 
+getTasks = () => {
+  let tasks = tasksInLocalStorage();
+  tasks.forEach((task) => {
+    if (task === '') {
+      return;
+    }
+    taskList.appendChild(newTask(task));
+  });
+};
+
+tasksInLocalStorage = () => {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  return tasks;
+};
+
 addTask = (e) => {
   e.preventDefault();
-  if (taskInput.value === '') {
+  task = taskInput.value;
+  if (task === '') {
     return;
   }
-  taskList.appendChild(newTask(taskInput.value));
+  taskList.appendChild(newTask(task));
+  addToLocalStorage(task);
   taskInput.value = '';
 };
 
@@ -28,22 +50,57 @@ newDeleteBtn = () => {
   return a;
 };
 
+addToLocalStorage = (task) => {
+  let tasks = tasksInLocalStorage();
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
 removeTask = (e) => {
   if (e.target.parentElement.classList.contains('delete-task')) {
     if (confirm('Are you sure?')) {
-      e.target.parentElement.parentElement.remove();
+      elem = e.target.parentElement.parentElement;
+      elem.remove();
+      removeFromLocalStorage(elem);
     }
   }
 };
 
+removeFromLocalStorage = (taskItem) => {
+  let tasks = tasksInLocalStorage();
+  tasks.forEach((task, index) => {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
 clearTasks = () => {
   while (taskList.firstChild) {
-    taskList.remove(taskList.firstChild);
+    taskList.removeChild(taskList.firstChild);
   }
+  localStorage.removeItem('tasks');
+};
+
+filterTasks = (e) => {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(
+    (task) => {
+      const item = task.firstChild.textContent;
+      if (item.toLowerCase().indexOf(text) == -1) {
+        task.style.display = 'none';
+      } else {
+        task.style.display = 'block';
+      }
+    }
+  );
 };
 
 (() => {
+  document.addEventListener('DOMContentLoaded', getTasks);
   form.addEventListener('submit', addTask);
   taskList.addEventListener('click', removeTask);
   clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 })();
